e532641ffc12a8ac98b41f23751a74bdedc5e793
Loralee Dawn <50155935+LoraleeSmith@users.noreply.github.com>
2019-12-03 21:30:22 -0800

    database

---
+++ Bamazon_db.sql
@@ -0,0 +1,83 @@
+CREATE bamazon_db;
+
+use bamazon_db;
+
+create table products(
+item_id 
+product_name 
+department_name
+price 
+stock_quantity 
+);
+
+create table products(
+item_id 
+product_name 
+department_name
+price 
+stock_quantity 
+);
+
+create table products(
+item_id 
+product_name 
+department_name
+price 
+stock_quantity 
+);
+
+create table products(
+item_id 
+product_name 
+department_name
+price 
+stock_quantity 
+);
+
+create table products(
+item_id 
+product_name 
+department_name
+price 
+stock_quantity 
+);
+
+create table products(
+item_id 
+product_name 
+department_name
+price 
+stock_quantity 
+);
+
+create table products(
+item_id 
+product_name 
+department_name
+price 
+stock_quantity 
+);
+
+create table products(
+item_id 
+product_name 
+department_name
+price 
+stock_quantity 
+);
+
+create table products(
+item_id 
+product_name 
+department_name
+price 
+stock_quantity 
+);
+
+create table products(
+item_id 
+product_name 
+department_name
+price 
+stock_quantity 
+);
