cd5dc630e727f3f57d76b22cb151f9415ac3ec8d
Loralee Dawn <50155935+LoraleeSmith@users.noreply.github.com>
2019-12-05 20:27:16 -0800

    updated files

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,25 @@
-bamazon
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+require("console.table");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+
+    user: "root",
+
+    password: "root",
+    database: "bamazon_db"
+});
+
+connection.connect(function (err) {
+    if (err) {
+        throw (err);
+    }
+    console.log("connected as id", connection.threadId);
+
+    connection.query("select * from products", function (err, res) {
+        console.log(res)
+    })
+    connection.end();
+});
--- bamazonManager.js
+++ bamazonManager.js
@@ -1,11 +1,15 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+require("console.table");
+
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
 
     user: "root",
 
-    password: "password",
-    database: "bamazon"
+    password: "root",
+    database: "bamazon_db"
 });
 
 connection.connect(function (err) {
